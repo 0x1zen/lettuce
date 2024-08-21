aaedad266529e13735ef0b569e4e520acfedb7c6
0x1zen <rajdubalwork@gmail.com>
2024-08-21 17:46:20 +0530

    Completed With Navbar

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,12 @@
+import logo from "./logo.png"
+import header_image from "./header_image.png"
+import basket_icon from "./basket_icon.png"
+import search_icon from "./search_icon.png"
+
+
+export const assets={
+    logo,
+    header_image,
+    basket_icon,
+    search_icon
+}
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Header.css"
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Get Your Favorite Dishes Delivered Here</h2>
+        <p>Satisfy your cravings by ordering the meals you love with just a click.
+        Enjoy a variety of delicious options delivered right to your doorstep.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
