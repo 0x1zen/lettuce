460eb7b7ae797d6d0e257ea1785c68a151321c2b
0x1zen <rajdubalwork@gmail.com>
2024-08-23 11:48:07 +0530

    Added assets,added structure to explore menu,added styling to explore menu

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -3,6 +3,16 @@ import header_image from "./header_image.png"
 import basket_icon from "./basket_icon.png"
 import search_icon from "./search_icon.png"
 
+// Menu images
+import salad from "./salad.jpg"
+import rolls from "./rolls.jpg"
+import sandwich from "./sandwich.jpg"
+import dessert from "./dessert.jpg"
+import cake from "./cake.jpg"
+import pureveg from "./pureveg.jpg"
+import pasta from "./pasta.jpg"
+import noodles from "./noodles.jpg"
+
 
 export const assets={
     logo,
@@ -10,3 +20,39 @@ export const assets={
     basket_icon,
     search_icon
 }
+
+
+export const menu_list=[
+    {
+        menu_name:"Salad",
+        menu_image:salad
+    },
+    {
+        menu_name:"rolls",
+        menu_image:rolls
+    },
+    {
+        menu_name:"sandwich",
+        menu_image:sandwich
+    },
+    {
+        menu_name:"dessert",
+        menu_image:dessert
+    },
+    {
+        menu_name:"cake",
+        menu_image:cake
+    },
+    {
+        menu_name:"noodles",
+        menu_image:noodles
+    },
+    {
+        menu_name:"pasta",
+        menu_image:pasta
+    },
+    {
+        menu_name:"pure veg",
+        menu_image:pureveg
+    },
+]
--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import "./ExploreMenu.css"
+import { menu_list } from '../../assets/assets'
 const ExploreMenu = () => {
   return (
-    <div>
-      
+    <div className='explore-menu' id='explore-menu'>
+        <h1>Explore Our Menu</h1>      
+        <p className='explore-menu-text'>Explore a vibrant array of dishes, each offering a unique blend of flavors, from traditional favorites to innovative culinary creations that cater to every palate.</p>
+        <div className='explore-menu-list'>
+          {menu_list.map((item,index)=>{
+            return(
+              <div key={index} className='explore-menu-list-item'>
+                <img src={item.menu_image}/>
+                <p>{item.menu_name}</p>
+              </div>
+            )
+          })}
+        </div>
+        <hr/>
     </div>
   )
 }
